dc92af77fcb26819a4b971626e4bd8bfdbfa6b13
yonatan-h <yon019hl@gmail.com>
2024-10-26 15:45:26 +0300

    setup basic express server

--- backend2/src/main.ts
+++ backend2/src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
