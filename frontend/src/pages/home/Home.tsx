a1a15a8b142e108082d973f72df552deb4a2215b
yonatan-h <yon019hl@gmail.com>
2024-10-27 23:01:25 +0300

    add home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,3 +1,27 @@
+import { FaShapes } from "react-icons/fa";
+import { GiNotebook } from "react-icons/gi";
+import PromptCard from "./components/PromptCard";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col gap-12">
+      <h1 className="font-bold text-xl">Get Started</h1>
+      <div className="flex flex-wrap gap-6">
+        <PromptCard
+          Icon={FaShapes}
+          title="Import Data"
+          description="Create a table by uploading local files"
+          to="/new-table"
+          toLabel="Create Table"
+        />
+        <PromptCard
+          Icon={GiNotebook}
+          title="Note Book"
+          description="Create a new notebook for data analysis, and machine learning."
+          to="new-notebook"
+          toLabel="Create Notebook"
+        />
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/pages/home/components/PromptCard.tsx
@@ -0,0 +1,32 @@
+import { IconType } from "react-icons";
+import { Link } from "react-router-dom";
+import { Button } from "../../../components/ui/button";
+
+export default function PromptCard({
+  Icon,
+  title,
+  description,
+  to,
+  toLabel,
+}: {
+  Icon: IconType;
+  title: string;
+  description: string;
+  to: string;
+  toLabel: string;
+}) {
+  return (
+    <div className="text-sm border shadow p-3 flex flex-col gap-3">
+      <div className="flex gap-3 items-center">
+        <div className="p-2 bg-blue-800 rounded">
+          <Icon className="text-background " />
+        </div>
+        <h3 className="font-bold">{title}</h3>
+      </div>
+      <p className="">{description}</p>
+      <Link to={to}>
+        <Button variant="outline">{toLabel}</Button>
+      </Link>
+    </div>
+  );
+}
