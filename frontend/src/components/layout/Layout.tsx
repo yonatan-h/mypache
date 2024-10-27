6cd329ca96f9cf6e4f5b10a5fb282c45db1873a9
yonatan-h <yon019hl@gmail.com>
2024-10-27 22:46:41 +0300

    logo for home page link

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineCloud } from "react-icons/ai";
 import { BsBricks } from "react-icons/bs";
 import { IoMdAddCircleOutline } from "react-icons/io";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { Button } from "../ui/button";
 import NavigationLink from "./NavigationLink";
 
@@ -9,10 +9,13 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="px-6 bg-foreground text-background flex justify-between items-center p-2">
-        <div className="flex items-center gap-3 text font-bold text-background/90 ">
+        <Link
+          to="/"
+          className="flex items-center gap-3 text font-bold text-background/90 "
+        >
           <BsBricks />
           mybricks
-        </div>
+        </Link>
         <Button className="bg-background/20 rounded-full w-8 h-8">Y</Button>
       </div>
       <div className="flex-1 flex ">
@@ -21,7 +24,7 @@ export default function Layout() {
              py-2 bg-foreground border-y text-background/90
             border-background/20 flex flex-col gap-2`}
         >
-          <NavigationLink Icon={IoMdAddCircleOutline} title="New" to="/" />
+          <NavigationLink Icon={IoMdAddCircleOutline} title="New" to="/new" />
           <NavigationLink Icon={AiOutlineCloud} title="Compute" to="/compute" />
         </nav>
         <main className="flex-1 p-6">
